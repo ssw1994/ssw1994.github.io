870995d1c003089c00622d0f37c1d9a5981985b8
ssw1994 <waghmare.sachin1994@gmail.com>
2018-11-15 19:35:58 +0530

    testing git hub pages

---
+++ node_modules/ngx-mask/app/ngx-mask/ngx-mask.module.d.ts
@@ -0,0 +1,9 @@
+import { ModuleWithProviders } from '@angular/core';
+import { optionsConfig } from './config';
+export declare class NgxMaskModule {
+    static forRoot(configValue?: optionsConfig): ModuleWithProviders;
+}
+/**
+ * @internal
+ */
+export declare function _configFactory(initConfig: optionsConfig, configValue: optionsConfig | (() => optionsConfig)): Function | optionsConfig;
